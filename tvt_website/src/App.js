0f3bb6a38781f02034d6f52a1349116eacb6960a
atharva-555 <atharva.bobade555@gmail.com>
2024-02-18 17:27:46 +0530

    third commit

--- tvt_website/src/App.js
+++ tvt_website/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header';
 import {BrowserRouter , Routes, Route} from 'react-router-dom';
 import Home from './pages/Home/index';
 import About from './pages/About/index';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- tvt_website/src/components/header/header.js
+++ tvt_website/src/components/header/header.js
@@ -2,8 +2,8 @@ import React from 'react';
 import '../header/header.css';
 import Logo from '../../assets/images/logo.svg';
 import { Link } from 'react-router-dom';
-
 import Button from '@mui/material/Button';
+
 
 const Header = () =>{
     return(
@@ -13,22 +13,26 @@ const Header = () =>{
                 <div className='row'>
 
                     {/* Logo */}
-                    <div className='col'>
+                    <div className='col-sm-2'>
                         <img className="logo" alt="" src={Logo}/>
                     </div>
 
-
+                    {/* GAP */}
+                    <div className='col-sm-5'></div>
                     {/* Navbar */}
-                    <div className='col'>
+                    <div className='col-sm-5'>
                         <ul className="list list-inline">
                             <li className="list-inline-item">
                                 <Button><Link>Home</Link></Button>
                             </li>
                             <li className="list-inline-item">
-                                <Button><Link>Home</Link></Button>
+                                <Button><Link>About Us</Link></Button>
                             </li>
                             <li className="list-inline-item">
-                                <Button><Link>Home</Link></Button>
+                                <Button><Link>Categories</Link></Button>
+                            </li> 
+                            <li className="list-inline-item">
+                                <Button><Link>Search</Link></Button>
                             </li>                             
                         </ul>
                     </div>
